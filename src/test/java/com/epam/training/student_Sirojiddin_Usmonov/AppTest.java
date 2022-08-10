c9b721642df2d184cc651b34c80ccea399474e40
Sirojiddin Usmonov <sirojiddinsmnv@gmail.com>
2022-08-10 13:27:38 +0500

    sending repository by intellij idea

---
+++ src/test/java/com/epam/training/student_Sirojiddin_Usmonov/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.training.student_Sirojiddin_Usmonov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
